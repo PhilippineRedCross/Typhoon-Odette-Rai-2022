edc275138b0bd62556b14e2eea764d5f71bdb580
ElyManzon <34364488+ElyManzon@users.noreply.github.com>
2022-01-18 12:04:21 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,20 @@
+// SET VARIABLES FOR THIS Dashboard
+
+var config_Title = 'Typhoon Odette (Rai) Operation';
+var config_LastUpdate = '11 January 2022';
+
+var config_author = 'Elaine Manzon';
+var config_email = 'elaine.manzon@redcross.org.ph';
+var config_skype = 'alcantaraelaineb';
+
+var config_sector_chart_height = 200;
+var config_sector_chart_width = 290;
+
+var config_organisation_chart_height = 200;
+var config_organisation_chart_width = 290;
+
+var config_service_chart_height = 300;
+var config_service_chart_width = 500;
+
+var config_map_height = 600;
+var config_map_width = 550;
